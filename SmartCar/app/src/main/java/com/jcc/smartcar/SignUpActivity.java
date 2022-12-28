e5597c6924faa108b388335a05edd0935d1703b5
LucijaMiroic <lmiroic1@student.foi.hr>
2022-12-28 01:01:02 +0100

    Design of the SignUpActivity made.

--- SmartCar/app/src/main/java/com/jcc/smartcar/SignUpActivity.java
+++ SmartCar/app/src/main/java/com/jcc/smartcar/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.jcc.smartcar;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,27 +9,16 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
-public class IntroActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
-    private Button btnSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
-
+        setContentView(R.layout.activity_sign_up);
         getWindow().setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
         );
-        btnSignUp = (Button)findViewById(R.id.btn_sign_up_intro);
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent signupIntent = new Intent(IntroActivity.this, SignUpActivity.class);
-                startActivity(signupIntent);
-            }
-        });
-
 
 
     }
