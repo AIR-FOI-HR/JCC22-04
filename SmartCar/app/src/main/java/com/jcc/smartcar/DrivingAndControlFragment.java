c518e5393af8b2281963f5b815d6091adae0fe78
Stella Lončarić <sloncari1@foi.hr>
2023-01-03 21:53:18 +0100

    Overview screen, Driving and Control screen and More screen are created as well as navigation bottom menu.

---
+++ SmartCar/app/src/main/java/com/jcc/smartcar/DrivingAndControlFragment.java
@@ -0,0 +1,20 @@
+package com.jcc.smartcar;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DrivingAndControlFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_driving_and_control, container, false);
+    }
+}
