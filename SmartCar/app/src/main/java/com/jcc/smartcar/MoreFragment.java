7ccecc09a4aa7a1b78d35048124dd1bcc6c0c421
ValentinaMarusevec1 <vmaruseve@foi.hr>
2023-01-04 18:52:40 +0100

    User logout is created and it works. The user is redirect to login screen.

--- SmartCar/app/src/main/java/com/jcc/smartcar/MoreFragment.java
+++ SmartCar/app/src/main/java/com/jcc/smartcar/MoreFragment.java
@@ -1,19 +1,43 @@
 package com.jcc.smartcar;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MoreFragment extends Fragment {
 
+    View root;
+    TextView logout;
+    FirebaseAuth auth;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false);
+        root = inflater.inflate(R.layout.fragment_more, container, false);
+        logout = root.findViewById(R.id.textViewLogOut);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(auth.getCurrentUser()!=null){
+                    auth.signOut();
+                    Intent intent = new Intent(getActivity(), LoginPageActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+        return  root;
     }
+
 }
