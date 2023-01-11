b267b22b5450c5d5ec23c996e57cd06c24a588b0
ValentinaMarusevec1 <vmaruseve@foi.hr>
2023-01-11 18:23:45 +0100

    Connecting Login textview with Login screen.

--- SmartCar/app/src/main/java/com/jcc/smartcar/SignUpActivity.java
+++ SmartCar/app/src/main/java/com/jcc/smartcar/SignUpActivity.java
@@ -1,10 +1,13 @@
 package com.jcc.smartcar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -16,7 +19,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends BaseActivity {
-
+    TextView login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +34,15 @@ public class SignUpActivity extends BaseActivity {
             @Override
             public void onClick(View view) {
                 registerUser();
+            }
+        });
+
+        login = findViewById(R.id.textViewLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, LoginPageActivity.class);
+                startActivity(intent);
             }
         });
     }
