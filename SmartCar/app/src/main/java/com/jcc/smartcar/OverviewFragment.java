90081370840ffaa9aaf9ee97f8150852f5d9cc2c
LucijaMiroic <lmiroic1@student.foi.hr>
2023-01-31 19:58:32 +0100

    Driving and control fragment almost done, some changes made in other activities and fragments.

--- SmartCar/app/src/main/java/com/jcc/smartcar/OverviewFragment.java
+++ SmartCar/app/src/main/java/com/jcc/smartcar/OverviewFragment.java
@@ -14,7 +14,7 @@ public class OverviewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_overview, container, false);
     }
 }
